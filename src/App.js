8ad473a7f523413f5e46135a3ef2ac57ac39d7b7
Mattia Centini <mattia.centini@gmail.com>
2020-10-05 09:28:49 +0200

    added css property for the effects

--- src/App.js
+++ src/App.js
@@ -3,6 +3,80 @@ import "./App.css";
 import Slider from "./components/Slider";
 import SidebarItem from "./components/SidebarItem";
 
+//setting the CSS values
+const DEFAULT_OPTIONS = [
+  {
+    name: "Brightness",
+    property: "brightness",
+    value: 100,
+    range: {
+      min: 0,
+      max: 200,
+    },
+    unit: "%",
+  },
+  {
+    name: "Contrast",
+    property: "contrast",
+    value: 100,
+    range: {
+      min: 0,
+      max: 200,
+    },
+    unit: "%",
+  },
+  {
+    name: "Saturation",
+    property: "saturation",
+    value: 100,
+    range: {
+      min: 0,
+      max: 200,
+    },
+    unit: "%",
+  },
+  {
+    name: "Grayscale",
+    property: "grayscale",
+    value: 0,
+    range: {
+      min: 0,
+      max: 100,
+    },
+    unit: "%",
+  },
+  {
+    name: "Sepia",
+    property: "sepia",
+    value: 0,
+    range: {
+      min: 0,
+      max: 100,
+    },
+    unit: "%",
+  },
+  {
+    name: "Hue Rotate",
+    property: "hue-rotate",
+    value: 0,
+    range: {
+      min: 0,
+      max: 360,
+    },
+    unit: "deg",
+  },
+  {
+    name: "Blur",
+    property: "blur",
+    value: 0,
+    range: {
+      min: 0,
+      max: 20,
+    },
+    unit: "px",
+  },
+];
+
 function App() {
   return (
     <div className="container">
